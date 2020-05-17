24630e2a29dc9632f519f2a363d78e952bd3eedc
Edison Arley Plaza Marin <edison954@gmail.com>
2020-05-17 11:06:04 -0500

    desestructuracion objetosyarreglos

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
 "use strict";
 (function () {
-    var miFuncion = function (a) {
-        return a.toUpperCase();
+    var avanger = {
+        nombre: "Steve",
+        clave: "Capitán América",
+        poder: "Droga",
     };
-    // function funcion2(a: string) {
-    //   return a;
-    // }
-    var miFuncionF = function (a) {
-        return a.toUpperCase();
-    };
-    var miFuncionF1 = function (a) { return a.toUpperCase(); };
-    console.log(miFuncion("normal"));
-    console.log(miFuncionF("flecha"));
-    console.log(miFuncionF1("flecha1"));
-    var sumarN = function (a, b) {
-        return a + b;
-    };
-    var sumarF = function (a, b) { return a + b; };
-    console.log(sumarN(8, 7));
-    console.log(sumarF(8, 7));
-    // const hulk = {
-    //   nombre: "hulk",
-    //   smash() {
-    //     console.log(`${this.nombre} smash!!!`);
-    //   },
+    // console.log(avanger.nombre);
+    // console.log(avanger.clave);
+    // console.log(avanger.poder);
+    // Desestructuracion de objetos
+    // const extraer = (avanger: any) => {
+    //   const { nombre, clave, poder } = avanger;
+    //   console.log(nombre);
+    //   console.log(clave);
+    //   console.log(poder);
     // };
-    // hulk.smash();
-    var hulk = {
-        nombre: "hulk",
-        smash: function () {
-            var _this = this;
-            setTimeout(function () {
-                console.log(_this.nombre + " smash!!!");
-            }, 1000);
-        },
+    // extraer(avanger);
+    var extraer = function (_a) {
+        //const { nombre, clave, poder } = avanger;
+        var nombre = _a.nombre, clave = _a.clave, poder = _a.poder;
+        console.log(nombre);
+        console.log(clave);
+        console.log(poder);
     };
-    hulk.smash();
+    //extraer(avanger);
+    //Desestructuracion de arreglos
+    var avangers = ["Thor", "Ironmnan", "Spiderman"];
+    console.log(avangers[0]);
+    console.log(avangers[1]);
+    console.log(avangers[2]);
+    var thor = avangers[0], aironman = avangers[1], spiderman = avangers[2];
+    console.log(thor);
+    console.log(aironman);
+    console.log(spiderman);
+    //--- con commas indicar que no mapee las posiciones que no necesitamos
+    var spiderman1 = avangers[2];
+    console.log(spiderman1);
+    //---con funcion
+    // const extraerArr = (avangers: string[]) => {
+    //   console.log(avangers[0]);
+    //   console.log(avangers[1]);
+    //   console.log(avangers[2]);
+    // };
+    // extraerArr(avangers);
+    var extraerArr = function (_a) {
+        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
+        console.log(thor);
+        console.log(ironman);
+        console.log(spiderman);
+    };
+    extraerArr(avangers);
 })();
