7223d75ae8eaade9594f58570ef363ff1385e9a1
Edison Arley Plaza Marin <edison954@gmail.com>
2020-05-17 17:23:28 -0500

    clases

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 "use strict";
 (() => {
-    //Interfaces
-    // const enviarMision = (xmen: any) => {
-    //   console.log(`Enviando a ${xmen.nombre} a la misión`);
-    // };
-    const enviarMision = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const regresarAlCuartel = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la misión`);
-    };
-    const wolverine = {
-        nombre: "Logan",
-        edad: 60,
-    };
-    enviarMision(wolverine);
-    regresarAlCuartel(wolverine);
+    //Clases
+    // class Avanger {
+    //   nombre: string;
+    //   equipo: string;
+    //   nombreReal?: string;
+    //   puedePelear: boolean;
+    //   peleasGanadas: number;
+    //   constructor(nombre: string, equipo: string) {
+    //     this.nombre = nombre;
+    //     this.equipo = equipo;
+    //   }
+    // }
+    class Avanger {
+        // nombre: string;
+        // equipo: string;
+        // nombreReal?: string;
+        // puedePelear: boolean;
+        // peleasGanadas: number;
+        constructor(equipo, nombre, nombreReal, puedePelear = true, peleasGanadas = 0) {
+            this.equipo = equipo;
+            this.nombre = nombre;
+            this.nombreReal = nombreReal;
+            this.puedePelear = puedePelear;
+            this.peleasGanadas = peleasGanadas;
+        }
+    }
+    //const antman: Avanger = new Avanger();
+    const antman = new Avanger("Antman", "Capi");
+    console.log(antman);
 })();
