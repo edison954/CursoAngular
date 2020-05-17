bb0f2442e8252bb92962f8002edfbd210a4795a0
Edison Arley Plaza Marin <edison954@gmail.com>
2020-05-17 10:33:51 -0500

    funciones flecha

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 "use strict";
 (function () {
-    function activar(quien, momento, objeto) {
-        if (objeto === void 0) { objeto = "batiseñal"; }
-        if (momento) {
-            console.log(quien + " activ\u00F3 la " + objeto + " en la " + momento);
-        }
-        else {
-            console.log(quien + " activ\u00F3 la " + objeto + ".");
-        }
-    }
-    // activar("Gordon", "batiseñal", "tarde");
-    activar("Gordon", "tarde");
+    var miFuncion = function (a) {
+        return a.toUpperCase();
+    };
+    // function funcion2(a: string) {
+    //   return a;
+    // }
+    var miFuncionF = function (a) {
+        return a.toUpperCase();
+    };
+    var miFuncionF1 = function (a) { return a.toUpperCase(); };
+    console.log(miFuncion("normal"));
+    console.log(miFuncionF("flecha"));
+    console.log(miFuncionF1("flecha1"));
+    var sumarN = function (a, b) {
+        return a + b;
+    };
+    var sumarF = function (a, b) { return a + b; };
+    console.log(sumarN(8, 7));
+    console.log(sumarF(8, 7));
+    // const hulk = {
+    //   nombre: "hulk",
+    //   smash() {
+    //     console.log(`${this.nombre} smash!!!`);
+    //   },
+    // };
+    // hulk.smash();
+    var hulk = {
+        nombre: "hulk",
+        smash: function () {
+            var _this = this;
+            setTimeout(function () {
+                console.log(_this.nombre + " smash!!!");
+            }, 1000);
+        },
+    };
+    hulk.smash();
 })();
