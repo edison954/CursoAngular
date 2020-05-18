aa5d84c35f2bdd960f4d92a2d7e22f28c8257a46
Edison Arley Plaza Marin <edison954@gmail.com>
2020-05-17 19:31:48 -0500

    fin introd a typescript

--- typescript-basico/app.ts
+++ typescript-basico/app.ts
@@ -1,12 +1,56 @@
 (() => {
-  const sumar = (a: number, b: number): number => a + b;
-  const nombre = (): string => "Hola Edison";
+  // Uso de Let y Const
+  const nombre = "Ricardo Tapia";
+  const edad = 23;
 
-  const obtenerSalario = (): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      resolve("Edison");
-    });
+  const PERSONAJE = { nombre, edad };
+
+  // Cree una interfaz que sirva para validar el siguiente objeto
+  interface ISuperHeroe {
+    nombre: string;
+    artesMarciales: string[];
+  }
+
+  const batman: ISuperHeroe = {
+    nombre: "Bruno Díaz",
+    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"],
   };
 
-  obtenerSalario().then((a) => console.log(a.toUpperCase()));
+  // Convertir esta funcion a una funcion de flecha
+  function resultadoDoble(a, b) {
+    return (a + b) * 2;
+  }
+
+  const constante = "este codigo no puede cambiar";
+
+  const resultadoDoble1 = (a: number, b: number) => (a + b) * 2;
+
+  // Función con parametros obligatorios, opcionales y por defecto
+  // donde NOMBRE = obligatorio
+  //       PODER  = opcional
+  //       ARMA   = por defecto = 'arco'
+  function getAvenger(nombre: string, poder?: string, arma: string = "arco") {
+    let mensaje;
+    if (poder) {
+      mensaje = nombre + " tiene el poder de: " + poder + " y un arma: " + arma;
+    } else {
+      mensaje = nombre + " tiene un " + poder;
+    }
+  }
+
+  // Cree una clase que permita manejar la siguiente estructura
+  // La clase se debe de llamar rectangulo,
+  // debe de tener dos propiedades:
+  //   * base
+  //   * altura
+  // También un método que calcule el área  =  base * altura,
+  // ese método debe de retornar un numero.
+
+  class Rectangulo {
+    constructor(public base: number, public altura: number) {}
+    // calcularArea() {
+    //   return this.base * this.altura;
+    // }
+    calcularArea = (): number => this.base * this.altura;
+  }
 })();
