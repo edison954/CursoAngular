27ccb1c1c0d5b333959b4cc48f5b2565bfee68f4
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-03 21:36:24 -0500

    pipes

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre: string = 'Edison Plaza';
+  arreglo: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Llego la data');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  };
+}
