e1723d78ad9061fefc779db4d718bb91ea04c38f
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-03 23:47:45 -0500

    pipes personalizados

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Edison Plaza';
+  nombre2: string = 'EdISon aRley pLAZa MArIn';
   arreglo: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
   fecha: Date = new Date();
+  idioma: string = 'fr';
+  videoUrl: stirng = 'https://www.youtube.com/embed/2Vv-BfVoq4g';
+  activar: boolean = true;
 
   valorPromesa = new Promise<string>((resolve) => {
     setTimeout(() => {
--- 03-pipes/src/app/app.module.ts
+++ 03-pipes/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
 
 registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
