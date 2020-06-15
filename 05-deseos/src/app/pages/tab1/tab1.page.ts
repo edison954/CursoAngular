92a1a3df65c65e26466b22d5c6df410a3ceaccf1
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-15 18:10:06 -0500

    lista de tareas, alert

--- 05-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { DeseosService } from './../../services/deseos.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +10,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor(public deseosService: DeseosService) { }
+  constructor(
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertCtrl: AlertController) {
+
+  }
+
+  async agregarLista() {
+    // this.router.navigateByUrl('tabs/tab1/agregar');
+
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log(data);
+            if (data.titulo.length === 0) {
+              return;
+            }
+            // Crear la lista
+            this.deseosService.crearLista(data.titulo);
+
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
+
 
 }
