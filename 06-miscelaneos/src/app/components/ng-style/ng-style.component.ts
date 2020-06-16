95ca5b0fbc536de43aa3bb09eee3f967679cf93a
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-16 00:52:34 -0500

    tamano

---
+++ 06-miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [ngStyle]="{'font-size': tamano + 'px'}">
+      Hola mundo... esta es una etiqueta
+    </p>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano: number = 20;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
