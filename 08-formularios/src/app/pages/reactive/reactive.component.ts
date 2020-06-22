a2e1a70fc360bff31c7cbad0b8e0eae40097b33d
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-22 10:05:23 -0500

    form reactivo poner en rojo controles

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -27,12 +27,22 @@ export class ReactiveComponent implements OnInit {
   get correoNoValido() {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
 
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
       apellido: ['', Validators.required],
-      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      })
     });
   }
 
@@ -41,8 +51,12 @@ export class ReactiveComponent implements OnInit {
 
     if (this.forma.invalid) {
       return Object.values(this.forma.controls).forEach(control => {
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach(control => control.markAsTouched());
+        } else {
+          control.markAsTouched();
+        }
         console.log(control);
-        control.markAsTouched();
       });
     }
 
