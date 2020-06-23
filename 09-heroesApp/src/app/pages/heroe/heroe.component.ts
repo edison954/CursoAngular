597b5c3eb5462d1db3b2553dcb4bab3ae8e014a0
Edison Arley Plaza Marin <edison954@gmail.com>
2020-06-22 23:58:48 -0500

    heroesapp, formulario y bakend firebase

---
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,31 @@
+import { HeroreModel } from './../../models/heroe.model';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe: HeroreModel = new HeroreModel();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(form: NgForm) {
+
+    if (form.invalid) {
+      console.log('formulario no valido');
+      return;
+    }
+
+    console.log(form);
+    console.log(this.heroe);
+  }
+
+
+}
