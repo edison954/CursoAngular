5e7ef4538ff143ac137c1fc8339d4f98b9880b2e
Edison Arley Plaza Marin <edison954@gmail.com>
2020-07-20 21:54:52 -0500

    mapas app material card

--- 12-mapas/src/app/app.module.ts
+++ 12-mapas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
