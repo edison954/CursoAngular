fb6c71aec09c92c58bc802130db5d4d4ac7d8b5e
Edison Arley Plaza Marin <edison954@gmail.com>
2020-07-26 17:38:40 -0500

    fotosfirebase

---
+++ 15-fotosfirebase/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { CargaImagenesService } from './services/carga-imagenes.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+// Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
+  ],
+  providers: [
+    CargaImagenesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
