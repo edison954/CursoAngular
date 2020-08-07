f6e2c8e7fb5b8b33303c12c03cc80bd7768b6efa
Edison Arley Plaza Marin <edison954@gmail.com>
2020-08-07 01:23:54 -0500

    firebase funcion demo request params

--- 18-goty/goty-backend/functions/src/index.ts
+++ 18-goty/goty-backend/functions/src/index.ts
@@ -2,8 +2,11 @@ import * as functions from 'firebase-functions';
 
 import * as admin from 'firebase-admin';
 
-admin.initializeApp({
+const serviceAccount = require("./serviceAccountKey.json");
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-grafica-4a698.firebaseio.com"
 });
 
 // // Start writing Firebase Functions
@@ -13,4 +16,11 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.json({
       mensaje: 'Hola Mundo desde Funciones Firebase!!!!!'});
+});
+
+export const getGOTY = functions.https.onRequest((request, response) => {
+    const nombre = request.query.nombre || 'sin nombre';
+    response.json({
+        nombre
+    })
 });
