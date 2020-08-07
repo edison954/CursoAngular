7e212f0e86bfe96301dad837083f6626990e51db
Edison Arley Plaza Marin <edison954@gmail.com>
2020-08-07 02:09:31 -0500

    firebase function uso de express peticion post

--- 18-goty/goty-backend/functions/src/index.ts
+++ 18-goty/goty-backend/functions/src/index.ts
@@ -2,12 +2,17 @@ import * as functions from 'firebase-functions';
 
 import * as admin from 'firebase-admin';
 
+import * as express from 'express';
+import * as cors from 'cors';
+
 const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://firestore-grafica-4a698.firebaseio.com"
 });
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -18,9 +23,59 @@ export const helloWorld = functions.https.onRequest((request, response) => {
       mensaje: 'Hola Mundo desde Funciones Firebase!!!!!'});
 });
 
-export const getGOTY = functions.https.onRequest((request, response) => {
-    const nombre = request.query.nombre || 'sin nombre';
-    response.json({
-        nombre
-    })
+export const getGOTY = functions.https.onRequest(  async (request, response) => {
+    // const nombre = request.query.nombre || 'sin nombre';
+    // response.json({
+    //     nombre
+    // })
+
+    const gotyRef = db.collection('goty');
+
+    const docsSnap = await gotyRef.get();
+
+    const juegos = docsSnap.docs.map( doc => doc.data());
+
+    response.json(juegos);
+
 });
+
+
+//express
+const app = express();
+app.use(cors({ origin:true }));
+
+app.get('/goty', async(req, res) => {
+
+    const gotyRef = db.collection('goty');
+
+    const docsSnap = await gotyRef.get();
+
+    const juegos = docsSnap.docs.map( doc => doc.data());
+
+    res.json(juegos);
+
+});
+
+app.post('/goty/:id', async(req, res) => {
+
+    const id = req.params.id;
+    const gameRef = db.collection('goty').doc( id );
+    const gameSnap = await gameRef.get();
+
+    if ( !gameSnap.exists ) {
+        res.status(404).json({
+            ok: false,
+            mensaje: 'No existe un juego con ese ID ' + id
+        })
+    } else {
+        res.json('Juego existe');
+    }
+
+
+});
+
+
+// exports.api = functions.https.onRequest(app);
+
+// otra forma
+export const api = functions.https.onRequest(app);
